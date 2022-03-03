a2bdf61df992e587fd39b9536145ee23dd0f04fc
Xiaohai Xu <xuxiaohai@microsoft.com>
2022-03-03 12:28:25 -0800

    SEKAI{wp_T5J_Ugra} (#9)
    * SEKAI{wp_T5J_Ugra}
    
    * SEKAI{f1x_tYp0}
    
    Co-authored-by: Eana Hufwe <ilove@1a23.com>

--- data/authors/lemon.md
+++ data/authors/lemon.md
@@ -3,8 +3,8 @@ name: lemon 蔡
 avatar: https://www.gravatar.com/avatar/6884dca105cdacb1eac9e58e468a860f?d=identicon&s=256
 specialties: [Web, Pwn]
 member: true
-description: Parrot Funclub
+description: Parrot Fanclub
 order: 7
 ---
 
-Parrot Funclub
+Parrot Fanclub
--- pages/404.tsx
+++ pages/404.tsx
@@ -12,7 +12,9 @@ export default function FourZeroFour() {
         <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
           Sorry we couldn't find this page.
         </p>
-        <p className="mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
+        <p className="mb-8">
+          But don't worry, you can find plenty of other things on our homepage.
+        </p>
         <Link href="/">
           <button className="inline px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-blue-600 border border-transparent rounded-lg shadow focus:outline-none focus:shadow-outline-blue hover:bg-blue-700 dark:hover:bg-blue-500">
             Back to homepage
