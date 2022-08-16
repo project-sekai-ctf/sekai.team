9cb835c99494a4749888984500da5cd35aa7fb27
Eana Hufwe <eana@1a23.com>
2022-08-15 21:34:17 -0700

    SEKAI{CTF2022}

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -3,6 +3,7 @@ const headerNavLinks = [
   { href: '/contests', title: 'Contests' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
+  { href: 'https://ctf.sekai.team/', title: 'CTF 2022' },
 ]
 
 export default headerNavLinks
