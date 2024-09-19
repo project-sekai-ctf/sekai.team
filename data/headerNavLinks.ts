8ea143c8f7508be52766cc3193d758c2d6a51b4a
sahuang <sahuang97@gmail.com>
2024-09-18 23:31:27 -0700

    SEKAI{ctf-2024}

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/members', title: 'Members' },
   { href: '/contests', title: 'Contests' },
   { href: '/blog', title: 'Blog' },
-  { href: 'https://ctf.sekai.team/', title: 'CTF 2024' },
+  { href: 'https://2024.ctf.sekai.team/', title: 'CTF 2024' },
 ]
 
 export default headerNavLinks
