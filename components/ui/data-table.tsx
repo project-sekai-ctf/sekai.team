1398f00218d3e85c244c5b806d611c6bb3330763
enscribe <jason@enscribe.dev>
2024-10-15 22:10:35 -0700

    fix: preflight overriding border color

--- components/ui/data-table.tsx
+++ components/ui/data-table.tsx
@@ -45,7 +45,7 @@ export function DataTable<TData, TValue>({
     },
   })
   return (
-    <div className="rounded-md border">
+    <div className="rounded-md border border-border">
       <Table>
         <TableHeader>
           {table.getHeaderGroups().map((headerGroup) => (
