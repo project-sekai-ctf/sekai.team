8b058ff4f9d5daaf38b2491c1f98e7cce6d703af
sahuang <sahuang@osec.io>
2026-06-30 20:50:37 +0800

    SEKAI{2026}

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/members', title: 'Members' },
   { href: '/contests', title: 'Contests' },
   { href: '/blog', title: 'Blog' },
-  { href: 'https://ctf.sekai.team/', title: 'CTF 2025' },
+  { href: 'https://ctf.sekai.team/', title: 'CTF 2026' },
 ]
 
 export default headerNavLinks
